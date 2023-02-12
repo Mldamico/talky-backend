11724c8f56dc0040da1524973fb5d865c67ada98
mldamico <matias.lioneldamico@gmail.com>
2023-02-12 13:44:36 -0300

    Signupdata and cloudinary for signup

--- src/features/auth/controllers/signup.ts
+++ src/features/auth/controllers/signup.ts
@@ -1,10 +1,13 @@
-import { ObjectId } from "mongoose";
+import { ObjectId } from "mongodb";
 import { Request, response } from "express";
 import { joiValidation } from "@global/decorators/joi-validation.decorators";
 import { signupSchema } from "@auth/schemes/signup";
-import { IAuthDocument } from "../interfaces/auth.interface";
+import { IAuthDocument, ISignUpData } from "../interfaces/auth.interface";
 import { authService } from "@service/db/auth.service";
 import { BadRequestError } from "@global/helpers/error-handler";
+import { Helpers } from "@global/helpers/helpers";
+import { UploadApiResponse } from "cloudinary";
+import { uploads } from "../../../shared/globals/helpers/cloudinary-upload";
 
 export class Signup {
   @joiValidation(signupSchema)
@@ -15,5 +18,40 @@ export class Signup {
     if (checkIfUserExists) {
       throw new BadRequestError("Invalid credentials");
     }
+
+    const authObjectId: ObjectId = new ObjectId();
+    const userObjectId: ObjectId = new ObjectId();
+    const uId = `${Helpers.generateRandomIntegers(12)}`;
+    const authData: IAuthDocument = Signup.prototype.signupData({
+      _id: authObjectId,
+      uId,
+      username,
+      email,
+      password,
+      avatarColor,
+    });
+    const result: UploadApiResponse = (await uploads(
+      avatarImage,
+      `${userObjectId}`,
+      true,
+      true
+    )) as UploadApiResponse;
+    if (!result?.public_id) {
+      throw new BadRequestError("File upload: Something wrong happened");
+    }
+  }
+
+  private signupData(data: ISignUpData): IAuthDocument {
+    const { _id, username, email, uId, password, avatarColor } = data;
+
+    return {
+      _id,
+      uId,
+      username: Helpers.firstLetterUppercase(username),
+      email: Helpers.lowerCase(email),
+      password,
+      avatarColor,
+      createdAt: new Date(),
+    } as IAuthDocument;
   }
 }
