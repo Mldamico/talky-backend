539dfba4a8282c84e604a56e28f7f167b28b521a
mldamico <matias.lioneldamico@gmail.com>
2023-02-21 19:18:36 -0300

    Several test in signup

--- src/features/auth/controllers/test/signup.test.ts
+++ src/features/auth/controllers/test/signup.test.ts
@@ -1,10 +1,16 @@
 import { Request, Response } from "express";
 import * as cloudinaryUploads from "@global/helpers/cloudinary-upload";
-import { authMockRequest, authMockResponse } from "@root/mocks/auth.mock";
+import {
+  authMock,
+  authMockRequest,
+  authMockResponse,
+} from "@root/mocks/auth.mock";
 
 import { CustomError } from "@global/helpers/error-handler";
 import { signupSchema } from "@auth/schemes/signup";
 import { Signup } from "@auth/controllers/signup";
+import { authService } from "@service/db/auth.service";
+import { UserCache } from "@service/redis/user.cache";
 
 jest.mock("@service/queues/base.queue");
 jest.mock("@service/queues/user.queue");
@@ -30,6 +36,187 @@ describe("SignUp", () => {
       expect(error.serializeErrors().message).toEqual(
         "Username is a required field"
       );
+    });
+  });
+  it("should throw an error if username length is less than minimum length", () => {
+    const req: Request = authMockRequest(
+      {},
+      {
+        username: "as",
+        email: "test@test.com",
+        password: "123456",
+        avatarColor: "color",
+        avatarImage: "data:text/plain;base64,SVGsbG8sIFdvcmxkIQ==",
+      }
+    ) as Request;
+    const res: Response = authMockResponse();
+    Signup.prototype.create(req, res).catch((error: CustomError) => {
+      expect(error.statusCode).toEqual(400);
+      expect(error.serializeErrors().message).toEqual("Invalid username");
+    });
+  });
+
+  it("should throw an error if username length is greather than maximum length", () => {
+    const req: Request = authMockRequest(
+      {},
+      {
+        username: "asasdafasfafsasd54a5d4a5sda5",
+        email: "test@test.com",
+        password: "123456",
+        avatarColor: "color",
+        avatarImage: "data:text/plain;base64,SVGsbG8sIFdvcmxkIQ==",
+      }
+    ) as Request;
+    const res: Response = authMockResponse();
+    Signup.prototype.create(req, res).catch((error: CustomError) => {
+      expect(error.statusCode).toEqual(400);
+      expect(error.serializeErrors().message).toEqual("Invalid username");
+    });
+  });
+
+  it("should throw an error if email is not valid", () => {
+    const req: Request = authMockRequest(
+      {},
+      {
+        username: "probando",
+        email: "g",
+        password: "123456",
+        avatarColor: "color",
+        avatarImage: "data:text/plain;base64,SVGsbG8sIFdvcmxkIQ==",
+      }
+    ) as Request;
+    const res: Response = authMockResponse();
+    Signup.prototype.create(req, res).catch((error: CustomError) => {
+      expect(error.statusCode).toEqual(400);
+      expect(error.serializeErrors().message).toEqual("Email must be valid");
+    });
+  });
+
+  it("should throw an error if email is not available", () => {
+    const req: Request = authMockRequest(
+      {},
+      {
+        username: "probando",
+        email: "",
+        password: "qwerty",
+        avatarColor: "red",
+        avatarImage: "data:text/plain;base64,SGVsbG8sIFdvcmxkIQ==",
+      }
+    ) as Request;
+    const res: Response = authMockResponse();
+    Signup.prototype.create(req, res).catch((error: CustomError) => {
+      expect(error.statusCode).toEqual(400);
+      expect(error.serializeErrors().message).toEqual(
+        "Email is a required field"
+      );
+    });
+  });
+
+  it("should throw an error if password is not available", () => {
+    const req: Request = authMockRequest(
+      {},
+      {
+        username: "probando",
+        email: "probando@test.com",
+        password: "",
+        avatarColor: "red",
+        avatarImage: "data:text/plain;base64,SGVsbG8sIFdvcmxkIQ==",
+      }
+    ) as Request;
+    const res: Response = authMockResponse();
+    Signup.prototype.create(req, res).catch((error: CustomError) => {
+      expect(error.statusCode).toEqual(400);
+      expect(error.serializeErrors().message).toEqual(
+        "Password is a required field"
+      );
+    });
+  });
+
+  it("should throw an error if password length is less than minimum length", () => {
+    const req: Request = authMockRequest(
+      {},
+      {
+        username: "probando",
+        email: "probando@test.com",
+        password: "ma",
+        avatarColor: "red",
+        avatarImage: "data:text/plain;base64,SGVsbG8sIFdvcmxkIQ==",
+      }
+    ) as Request;
+    const res: Response = authMockResponse();
+    Signup.prototype.create(req, res).catch((error: CustomError) => {
+      expect(error.statusCode).toEqual(400);
+      expect(error.serializeErrors().message).toEqual("Invalid password");
+    });
+  });
+
+  it("should throw an error if password length is greater than maximum length", () => {
+    const req: Request = authMockRequest(
+      {},
+      {
+        username: "probando",
+        email: "probando@test.com",
+        password: "mathematics12345678",
+        avatarColor: "red",
+        avatarImage: "data:text/plain;base64,SGVsbG8sIFdvcmxkIQ==",
+      }
+    ) as Request;
+    const res: Response = authMockResponse();
+    Signup.prototype.create(req, res).catch((error: CustomError) => {
+      expect(error.statusCode).toEqual(400);
+      expect(error.serializeErrors().message).toEqual("Invalid password");
+    });
+  });
+
+  it("should throw unauthorize error is user already exist", () => {
+    const req: Request = authMockRequest(
+      {},
+      {
+        username: "probando",
+        email: "probando@test.com",
+        password: "123456",
+        avatarColor: "red",
+        avatarImage: "data:text/plain;base64,SGVsbG8sIFdvcmxkIQ==",
+      }
+    ) as Request;
+    const res: Response = authMockResponse();
+    jest
+      .spyOn(authService, "getUserByUsernameOrEmail")
+      .mockResolvedValue(authMock);
+    Signup.prototype.create(req, res).catch((error: CustomError) => {
+      expect(error.statusCode).toEqual(400);
+      expect(error.serializeErrors().message).toEqual("Invalid credentials");
+    });
+  });
+  it("should set session data for valid credentials and send correct json response", async () => {
+    const req: Request = authMockRequest(
+      {},
+      {
+        username: "Manny",
+        email: "manny@test.com",
+        password: "qwerty",
+        avatarColor: "red",
+        avatarImage: "data:text/plain;base64,SGVsbG8sIFdvcmxkIQ==",
+      }
+    ) as Request;
+    const res: Response = authMockResponse();
+
+    jest
+      .spyOn(authService, "getUserByUsernameOrEmail")
+      .mockResolvedValue(null as any);
+    const userSpy = jest.spyOn(UserCache.prototype, "saveUserToCache");
+    jest
+      .spyOn(cloudinaryUploads, "uploads")
+      .mockImplementation((): any =>
+        Promise.resolve({ version: "1234737373", public_id: "123456" })
+      );
+
+    await Signup.prototype.create(req, res);
+    expect(req.session?.jwt).toBeDefined();
+    expect(res.json).toHaveBeenCalledWith({
+      message: "User created successfully",
+      user: userSpy.mock.calls[0][2],
+      token: req.session?.jwt,
     });
   });
 });
